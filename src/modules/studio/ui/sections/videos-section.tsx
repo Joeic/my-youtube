4c4c7930cf43aae76db5f847021e7de057783e48
Joeic <cqy1508460399@gmail.com>
2025-02-25 15:31:19 +0800

    add error boundry to video studio

--- src/modules/studio/ui/sections/videos-section.tsx
+++ src/modules/studio/ui/sections/videos-section.tsx
@@ -2,8 +2,20 @@
 
 import { DEFAULT_LIMIT } from "@/constans";
 import { trpc } from "@/trpc/client";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 
 export const VideoSection = () => {
+    return(
+        <Suspense fallback={<p>Loading...</p>}>
+            <ErrorBoundary fallback={<p>Error</p>}>
+                <VideoSectionSuspense />
+            </ErrorBoundary>
+        </Suspense>
+    )
+}
+
+const VideoSectionSuspense = () => {
     const [data] = trpc.studio.getMany.useSuspenseInfiniteQuery({
         limit: DEFAULT_LIMIT, 
 },{
