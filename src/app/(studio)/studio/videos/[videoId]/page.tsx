dbac099fd11a7943cd9956468722c7bde58d2002
Joeic <cqy1508460399@gmail.com>
2025-02-27 17:55:17 +0800

    initialze new page for playing video

--- src/app/(studio)/studio/videos/[videoId]/page.tsx
+++ src/app/(studio)/studio/videos/[videoId]/page.tsx
@@ -1,8 +1,19 @@
- const Page = () => {
+import { VideoView } from "@/modules/studio/ui/views/video-view";
+import { HydrateClient, trpc } from "@/trpc/server";
+
+export const dynamic = "force-dynamic";
+
+interface PageProps {
+    params: Promise<{videoId: string}>;
+};
+
+const Page = async({params} : PageProps) => {
+    const { videoId } = await params;
+    void trpc.studio.getOne({id: videoId});
     return (
-      <div>
-        video Id
-      </div>
+        <HydrateClient>
+        <VideoView videoId={videoId} />
+       </HydrateClient>
     );
 };
 
