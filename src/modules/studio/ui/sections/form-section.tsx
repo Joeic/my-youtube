499883f076e546d766523bd5b80e6dd461b19427
Joeic <cqy1508460399@gmail.com>
2025-02-27 18:22:22 +0800

    add dropdown menu

--- src/modules/studio/ui/sections/form-section.tsx
+++ src/modules/studio/ui/sections/form-section.tsx
@@ -1,9 +1,16 @@
 "use client"
 
+import { Button } from "@/components/ui/button";
 import { trpc } from "@/trpc/client";
-import { VideoIcon } from "lucide-react";
+import { icons, MoreVerticalIcon, TrashIcon, VideoIcon } from "lucide-react";
 import { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger
+    } from "@/components/ui/dropdown-menu"
 
 interface ForSectionProps {
     videoId: string;
@@ -21,20 +28,41 @@ export const FormSection = ({videoId}: ForSectionProps) => {
 const FormSectionSuspense = ({videoId} : ForSectionProps) => {
     const [video] = trpc.studio.getOne.useSuspenseQuery({id: videoId});
     return(
-        <div>
-            {JSON.stringify(video)}
+        <div className="flex items-center justify-between mb-6">
+           <div>
+                <h1 className="text-2xl font-bold">
+                    Video Details
+                </h1>
+                <p className="text-xs text-muted-foreground ">Manage your video detauls</p>
+            </div>
+
+            <div className="flex items-center gap-x-2">
+                <Button type="submit" disabled={false}>
+                    Save
+                </Button>
+                <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                        <Button variant="ghost" size="icon">
+                            <MoreVerticalIcon />
+                        </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent align="start">
+                        <DropdownMenuItem>
+                            <TrashIcon className="size-4 mr-2"/>
+                            Delete
+                        </DropdownMenuItem>
+                    </DropdownMenuContent>
+                </DropdownMenu>
+            </div>
         </div>
-    )
+    );
 
 };
 
 const FormSectionSkeleton = () => {
     return(
-        <div>
             <p>
             Loading
             </p>
-            
-        </div>
     );
 };
