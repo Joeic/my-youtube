70583c0644781bd2b4c279eb96186a22b141e468
Joeic <cqy1508460399@gmail.com>
2025-02-28 18:09:49 +0800

    display thumbanil at video page

--- src/modules/studio/ui/sections/form-section.tsx
+++ src/modules/studio/ui/sections/form-section.tsx
@@ -37,6 +37,8 @@ import { VideoPlayer } from "@/modules/videos/ui/components/video-player";
 import Link from "next/link";
 import { snakeCaseToTitle } from "@/lib/utils";
 import { useRouter } from "next/navigation";
+import Image from "next/image";
+
 
 interface FormSectionProps {
     videoId: string;
@@ -176,7 +178,27 @@ const FormSectionSuspense = ({videoId} : FormSectionProps) => {
                                 </FormItem>
                             )}
                         />
-                        {/** add thumbnail here */}
+                        <FormField
+                            name="thumbnailUrl"
+                            control={form.control}
+                            render={() => (
+                                <FormItem>
+                                    <FormLabel>
+                                        Thambnail
+                                    </FormLabel>
+                                    <FormControl>
+                                        <div className="p-0.5 border border-dashed border-neutral-400 relative h-[84px] w-[153px] group">
+                                          <Image
+                                            src={video.thumbnailUrl ?? "/placeholder.png"}
+                                            className="object-cover"
+                                            fill
+                                            alt="Thumbnail"
+                                           />
+                                        </div>
+                                    </FormControl>
+                                </FormItem>
+                            )}
+                         />
                         <FormField 
                             control={form.control}
                             name="categoryId"
