3591a7fc8a78bcdfb3482d99ffa980ae378ee9f4
Joeic <cqy1508460399@gmail.com>
2025-03-03 15:31:17 +0800

    update thumbnail to db

--- src/modules/studio/ui/components/thumbnail-upload-modal.tsx
+++ src/modules/studio/ui/components/thumbnail-upload-modal.tsx
@@ -1,4 +1,6 @@
 import { ResponsiveModal } from "@/components/responsive-dialog";
+import { UploadDropzone } from "../../../../lib/uploadthing";
+import { trpc } from "@/trpc/client";
 
 interface ThumbnailUploadModelProps{
     videoId: string;
@@ -11,13 +13,24 @@ export const ThumbnailUploadModel = ({
     open,
     onOpenChange,
 }:ThumbnailUploadModelProps) =>{
+    const utils = trpc.useUtils();
+
+    const onUploadComplete = () => {
+        utils.studio.getOne.invalidate({ id: videoId});
+        utils.studio.getMany.invalidate();
+        onOpenChange(false);
+    }
     return(
         <ResponsiveModal
             title="Upload a thumbanil"
             open={open}
             onOpenChange={onOpenChange}
         >
-            <p>Hello</p>
+           <UploadDropzone
+                endpoint="thumbnailUploader"
+                input={{videoId}}
+                onClientUploadComplete={onUploadComplete}
+            />
         </ResponsiveModal>
     )
 }
