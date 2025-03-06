426408a9d5f69ae32d29543a605763a8ae444fdb
Joeic <cqy1508460399@gmail.com>
2025-03-06 15:55:46 +0800

    add video page

---
+++ src/app/(home)/videos/[videoId]/page.tsx
@@ -0,0 +1,22 @@
+import { VideoView } from "@/modules/videos/views/video-view";
+import { trpc } from "@/trpc/server";
+
+interface PageProps{
+    params: Promise<{
+        videoId: string;
+    }>;
+}
+
+
+const Page = async ({ params }:PageProps) => {
+    const {videoId} = await params;
+
+    void trpc.videos.getOne.prefetch({id: videoId});
+    return (
+        <div>
+            <VideoView videoId={videoId}/>
+        </div>
+    );
+};
+
+export default Page;
