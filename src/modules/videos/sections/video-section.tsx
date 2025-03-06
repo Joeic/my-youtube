e58e1ab45f153340faca9b127269a1ec9fa5e46e
Joeic <cqy1508460399@gmail.com>
2025-03-06 16:09:57 +0800

    add video player to video page

--- src/modules/videos/sections/video-section.tsx
+++ src/modules/videos/sections/video-section.tsx
@@ -1,8 +1,10 @@
 "use client"
 
+import { cn } from "@/lib/utils";
 import { trpc } from "@/trpc/client";
 import { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
+import { VideoPlayer } from "../ui/components/video-player";
 
 interface VideoSectionProps{
     videoId: string;
@@ -25,9 +27,20 @@ const VideoSectionSuspense = ({videoId}: VideoSectionProps) => {
     const [video] = trpc.videos.getOne.useSuspenseQuery({id: videoId});
 
     return(
-        <div>
-            {JSON.stringify(video)}
-        </div>
+        <>
+            <div className={cn(
+            "aspect-video bg-black rounded-xl overflow-hidden relative",
+            video.muxStatus !== "ready" && "rounded-b-none",)}>
+                <VideoPlayer 
+                    autoPlay
+                    onPlay={() => {}}
+                    playbackId={video.muxPlaybackId}
+                    thumbnailUrl={video.thumbnailUrl}
+                />
+            </div>
+            {/* <VideoBanner status = {video.muxStatus/> */}
+        </>
+      
     )
 }
 
