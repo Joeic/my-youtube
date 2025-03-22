697eeb7a6f636de172d01ad2c7ff58f3dd9d21bf
Joeic <cqy1508460399@gmail.com>
2025-03-22 13:35:57 -0600

    add share button and description of videos under video page

--- src/modules/videos/ui/components/video-top-row.tsx
+++ src/modules/videos/ui/components/video-top-row.tsx
@@ -1,6 +1,10 @@
 import { retryLink } from "@trpc/client";
 import { VideoGetOneOutput } from "../../types";
 import { VideoOwner } from "./video-owner";
+import { VideoReactions } from "./video-reactions";
+import { VideoMenu } from "./video-menu";
+import { VideoDesciption } from "./video-description";
+import { useMemo } from "react";
 
 interface VideoSectionProps{
     video: VideoGetOneOutput;
@@ -9,12 +13,35 @@ interface VideoSectionProps{
 export const VideoTopRow = ({video}
     : VideoSectionProps
 ) =>{
+
+    const compactViews = useMemo( () => {
+        return Intl.NumberFormat("en",{
+            notation: "compact"
+        }).format(1000);
+    }, []);
+
+    const expandedViews = useMemo( () =>{
+        return Intl.NumberFormat("en",{
+            notation: "standard"
+        }).format(1000);
+    }, []);
     return(
         <div className="flex flex-col gap-4 mt-4">
             <h1 className="text-xl font-semibold">{video.title}</h1>
             <div className="flex flex-col sm:flex-row sm:items-start sm:justify-between gap-4">
                 <VideoOwner user={video.user} videoId={video.id}/>
+                <div className="flex overflow-x-auto sm:min-w[calc(50%-6px)] sm:justify-end
+                sm:overflow-visible pb-2 -mb-2 sm:pb-0 sm:mb-0 gap-2">
+                    <VideoReactions />
+                    <VideoMenu videoId={video.id} variant="secondary" />
+                </div>
             </div>
+            <VideoDesciption
+                compactViews={compactViews}
+                expandedViews={expandedViews}
+                compactDate="22/22/22"
+                expandedDate="12th Jan 2025"
+                description={video.description}/>
         </div>  
     )
 }
