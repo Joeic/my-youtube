944126946b18c9ee54fc1436682daf0980e6c32a
Joeic <cqy1508460399@gmail.com>
2025-03-23 16:16:32 -0600

    fix the typo of '${data.first_name} ${data.last_name}'

--- src/app/api/users/webhook/route.ts
+++ src/app/api/users/webhook/route.ts
@@ -76,7 +76,7 @@ export async function POST(req: Request) {
   if(eventType == "user.updated"){
     const {data} = evt;
     await db.update(users).set({
-        name: '${data.first_name} ${data.last_name}',
+        name: `${data.first_name} ${data.last_name}`,
         imageUrl: data.image_url,
     }).where(eq(users.clerkId, data.id));
   }
