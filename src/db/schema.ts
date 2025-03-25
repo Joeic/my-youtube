8fabbc399defd7fd74727fc046f335e2079c00af
Joeic <cqy1508460399@gmail.com>
2025-03-25 12:20:50 -0600

    add parent comment to comment table

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,6 +1,6 @@
 
 import { Description } from "@radix-ui/react-toast";
-import { pgTable, uuid, text,timestamp,uniqueIndex, integer, pgEnum,primaryKey} from "drizzle-orm/pg-core";
+import { pgTable, uuid, text,timestamp,uniqueIndex, integer, pgEnum,primaryKey, foreignKey} from "drizzle-orm/pg-core";
 import {relations} from "drizzle-orm";
 import { MuxUploaderStatus } from "@mux/mux-uploader-react";
 import{
@@ -127,11 +127,20 @@ export const videoRelations = relations(videos, ({one,many}) => ({
 
 export const comments = pgTable("comments",{
     id: uuid("id").primaryKey().defaultRandom(),
+    parentId: uuid("parent_id"),
     userId: uuid("user_id").references( () => users.id, {onDelete: "cascade"}).notNull(),
     videoId: uuid("video_id").references( () => videos.id, {onDelete: "cascade"}).notNull(),
     value: text("value").notNull(),
     createdAt: timestamp("created_at").defaultNow().notNull(),
     updatedAt: timestamp("updated_at").defaultNow().notNull(),
+}, (t) => {
+    return[
+        foreignKey({
+            columns: [t.parentId],
+            foreignColumns: [t.id],
+            name: "comments_parent_id_fkey",
+        }).onDelete("cascade"),
+    ]
 });
 
 export const commentRelations = relations(comments, ({one,many}) => ({
@@ -143,7 +152,15 @@ export const commentRelations = relations(comments, ({one,many}) => ({
         fields: [comments.videoId],
         references: [videos.id],
     }),
-    reactions:many(commentReactions)
+    parent: one(comments,{
+        fields: [comments.parentId],
+        references: [comments.id],
+        relationName: "comments_parent_id_fkey",
+    }),
+    reactions:many(commentReactions),
+    replies:many(comments,{
+        relationName: "comments_parent_id_fkey",
+    }),
 }))
 export const commentsInsertSchema = createInsertSchema(comments);
 export const commentsUpdateSchema = createUpdateSchema(comments);
