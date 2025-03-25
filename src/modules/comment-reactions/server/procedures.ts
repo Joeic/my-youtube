8e0745232e3e784a5c61670dc6c791f52b5e6380
Joeic <cqy1508460399@gmail.com>
2025-03-24 21:25:31 -0600

    created like and dislike action for comments

---
+++ src/modules/comment-reactions/server/procedures.ts
@@ -0,0 +1,97 @@
+import { db } from "@/db";
+import { commentReactions, videoReactions} from "@/db/schema";
+import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
+import { eq, and} from "drizzle-orm";
+import { abort } from "process";
+import { z } from "zod";
+
+export const commentReactionsRouter = createTRPCRouter({
+    like: protectedProcedure
+        .input(z.object({commentId: z.string().uuid()}))
+        .mutation(async ({input, ctx}) =>{
+            const {commentId} = input;
+            const {id: userId} = ctx.user;
+
+            const [existingCommentReactionsLike] = await db
+                .select()
+                .from(commentReactions)
+                .where(
+                    and(
+                        eq(commentReactions.commentId, commentId),
+                        eq(videoReactions.userId, userId),
+                        eq(videoReactions.type, "like"),
+                    )
+                );
+            if(existingCommentReactionsLike){
+                const [deleteViewerRection] = await db
+                    .delete(commentReactions)
+                    .where(
+                        and(
+                            eq(commentReactions.userId, userId),
+                            eq(commentReactions.commentId, commentId)
+                        )
+                    ).returning();
+            return deleteViewerRection;
+            }
+
+            const [createdCommentReaction] = await db
+                .insert(commentReactions)
+                .values({userId,commentId, type:"like"})
+                // if user already click dislike, it will update it to be like, 
+                // if we do not do conflict update, then it will insert duplicate 
+                // key for userId + videoId(even though the inserted type is like)
+                .onConflictDoUpdate({
+                    target: [commentReactions.userId, commentReactions.commentId],
+                    set: {
+                        type: "like",
+                    },
+                })
+                .returning();
+            return createdCommentReaction;
+        }),
+
+    dislike: protectedProcedure
+    .input(z.object({commentId: z.string().uuid()}))
+    .mutation(async ({input, ctx}) =>{
+        const {commentId} = input;
+        const {id: userId} = ctx.user;
+
+        const [existingCommentReactionDisLike] = await db
+            .select()
+            .from(commentReactions)
+            .where(
+                and(
+                    eq(commentReactions.commentId, commentId),
+                    eq(commentReactions.userId, userId),
+                    eq(commentReactions.type, "dislike"),
+                )
+            );
+        if(existingCommentReactionDisLike){
+            const [deleteViewerRection] = await db
+                .delete(commentReactions)
+                .where(
+                    and(
+                        eq(commentReactions.userId, userId),
+                        eq(commentReactions.commentId, commentId)
+                    )
+                ).returning();
+        return deleteViewerRection;
+        }
+
+        const [createdCommentReaction] = await db
+            .insert(commentReactions)
+            .values({userId, commentId, type:"dislike"})
+            // if user already click dislike, it will update it to be like, 
+            // if we do not do conflict update, then it will insert duplicate 
+            // key for userId + videoId(even though the inserted type is like)
+            .onConflictDoUpdate({
+                target: [commentReactions.userId, commentReactions.commentId],
+                set: {
+                    type: "dislike",
+                },
+            })
+            .returning();
+        return createdCommentReaction;
+    }),
+        
+})
