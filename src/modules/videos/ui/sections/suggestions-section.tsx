eb79d6a2e041ff64cb6d871507320e4658383635
Joeic <cqy1508460399@gmail.com>
2025-03-27 13:31:32 -0600

    fix the issue that al;l suggestions use the same videoId

--- src/modules/videos/ui/sections/suggestions-section.tsx
+++ src/modules/videos/ui/sections/suggestions-section.tsx
@@ -25,7 +25,7 @@ export const SuggestionsSection = ({videoId}: SuggestionsSectionProps) =>{
                 {suggestions.pages.flatMap( (page) => page.items.map( (video) => (
                     <VideoRowCard 
                     data={video}
-                    key={videoId}
+                    key={video.id}
                     size="compact"
                     />
                 )))}
@@ -34,7 +34,7 @@ export const SuggestionsSection = ({videoId}: SuggestionsSectionProps) =>{
                 {suggestions.pages.flatMap( (page) => page.items.map( (video) => (
                     <VideoGridCard 
                     data={video}
-                    key={videoId}
+                    key={video.id}
                     />
                 )))}
             </div>
