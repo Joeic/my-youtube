0ef8dcde5a2fd7b1748974e34f2c7e596e09a7f2
Joeic <cqy1508460399@gmail.com>
2025-03-27 20:03:25 -0600

    revalidate the video if the video upload failed

--- src/modules/studio/ui/sections/form-section.tsx
+++ src/modules/studio/ui/sections/form-section.tsx
@@ -112,6 +112,20 @@ const FormSectionSuspense = ({videoId} : FormSectionProps) => {
         }
     });
 
+    const revalidate = trpc.videos.revalidate.useMutation({
+        onSuccess: () => {
+            utils.studio.getMany.invalidate();
+            utils.studio.getOne.invalidate({id: videoId});
+            toast.success("Video revalidated");
+        },
+        onError: () => {
+           toast.error("something went wrong");
+            
+        }
+    });
+
+
+
     const restoreThumbnail = trpc.videos.restoreThumbnail.useMutation({
         onSuccess: () => {
             utils.studio.getMany.invalidate();
@@ -182,6 +196,10 @@ const FormSectionSuspense = ({videoId} : FormSectionProps) => {
                                     </Button>
                                 </DropdownMenuTrigger>
                                 <DropdownMenuContent align="end" side="left">
+                                    <DropdownMenuItem onClick={() => revalidate.mutate({id: videoId})}>
+                                        <RotateCcwIcon className="size-4 mr-2"/>
+                                            Revalidate
+                                    </DropdownMenuItem>
                                     <DropdownMenuItem onClick={() => remove.mutate({id: videoId})}>
                                         <TrashIcon className="size-4 mr-2"/>
                                             Delete
