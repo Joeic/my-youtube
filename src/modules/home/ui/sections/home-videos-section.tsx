266840badd6e66181784946710749fd43da3d60a
Joeic <cqy1508460399@gmail.com>
2025-03-29 12:36:35 -0600

    dispay videogrid card for videos

--- src/modules/home/ui/sections/home-videos-section.tsx
+++ src/modules/home/ui/sections/home-videos-section.tsx
@@ -1,6 +1,8 @@
 "use client"
 
+import { InfiniteScroll } from "@/components/infinite-scroll";
 import { DEFAULT_LIMIT } from "@/constans";
+import { VideoGridCard, VideoGridCardSkeleton } from "@/modules/videos/ui/components/video-grid-card";
 import { trpc } from "@/trpc/client";
 import { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
@@ -11,7 +13,7 @@ interface HomeVideosSectionProp {
 
 export const HomeVideosSection = (props: HomeVideosSectionProp) => {
     return(
-        <Suspense fallback={<HomeVideoSectionSkeleton />}>
+        <Suspense key ={props.categoryId} fallback={<HomeVideoSectionSkeleton />}>
             <ErrorBoundary fallback={<p>Error</p>}>
                 <HomeVideosSectionSuspense {...props}/>
             </ErrorBoundary>
@@ -21,8 +23,11 @@ export const HomeVideosSection = (props: HomeVideosSectionProp) => {
 
 const HomeVideoSectionSkeleton =() => {
     return(
-        <div>
-            Loading
+        <div className="gap-4 gap-y-10 grid grid-cols-1 sm: grid-cols-2 lg: grid-cols-3 xl: grid-cols-3 2xl:grid-cols-4 
+        [@media(min-width:1920px)]:grid-cols-5  [@media(min-width:1920px)]:grid-cols-6">
+            {Array.from({length: 18}).map( (_,index) => (
+            <VideoGridCardSkeleton key={index} />
+            ))}
         </div>
     )
 }
@@ -36,8 +41,19 @@ const HomeVideosSectionSuspense = ({categoryId}: HomeVideosSectionProp) => {
 
     return(
         <div>
-            {JSON.stringify(videos)}
+            <div className="gap-4 gap-y-10 grid grid-cols-1 sm: grid-cols-2 lg: grid-cols-3 xl: grid-cols-3 2xl:grid-cols-4 
+            [@media(min-width:1920px)]:grid-cols-5  [@media(min-width:1920px)]:grid-cols-6">
+                {videos.pages.flatMap( (page) => page.items).map( (video) => (
+                <VideoGridCard key={video.id} data={video}  />
+                ))}
+            </div>
+            <InfiniteScroll 
+                hasNextPage={query.hasNextPage}
+                isFetchingNextPage={query.isFetchingNextPage}
+                fetchNextPage={query.fetchNextPage}
+            />
         </div>
+        
     )
 
 }
