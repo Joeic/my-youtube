1069977c20433b4f4b4256f99c63d7a50004e7b1
Joeic <cqy1508460399@gmail.com>
2025-03-30 14:28:43 -0600

    create playlistcreatemodal

--- src/modules/playlists/ui/views/playlists-view.tsx
+++ src/modules/playlists/ui/views/playlists-view.tsx
@@ -4,10 +4,16 @@ import { Button } from "@/components/ui/button"
 import { HistoryVideosSection } from "../sections/history-videos-section"
 import { LikedVideosSection } from "../sections/liked-videos-section"
 import { PlusIcon } from "lucide-react"
+import { PlaylistCreateModel } from "../components/playlist-create-modal"
+import { useState } from "react"
 
 export const PlaylistsView = () => {
+
+    const [createModalOpen, setCreateModalOpen] = useState(false);
+
     return (
         <div className="max-w-[2400px] mx-auto mb-10 px-4 pt-2.5 flex flex-col gap-y-6">
+            <PlaylistCreateModel open={createModalOpen} onOpenChange={setCreateModalOpen} />
             <div className="flex justify-between items-center">
                 <div>
                     <h1 className=" text-2xl font-bold">
@@ -22,7 +28,7 @@ export const PlaylistsView = () => {
                 variant="outline"
                 size="icon"
                 className="rounded-full"
-                onClick={() => {}}
+                onClick={() => setCreateModalOpen(true)}
             >
                 <PlusIcon />
                 
