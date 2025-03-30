b356ab50f9447f4da3ca4bcc084fcbba98e24d43
Joeic <cqy1508460399@gmail.com>
2025-03-30 16:47:34 -0600

    add loader icon

--- src/modules/playlists/ui/components/playlist-add-modal.tsx
+++ src/modules/playlists/ui/components/playlist-add-modal.tsx
@@ -1,6 +1,7 @@
 import { ResponsiveModal } from "@/components/responsive-dialog";
 import { DEFAULT_LIMIT } from "@/constans";
 import { trpc } from "@/trpc/client";
+import { Loader2Icon } from "lucide-react";
 import {z} from "zod";
 
 interface PlaylistAddModelProps{
@@ -18,7 +19,7 @@ export const PlaylistAddModel = ({
     onOpenChange,
     videoId,
 }:PlaylistAddModelProps) =>{
-    const { data}  = trpc.playlists.getManyForVideo.useInfiniteQuery({limit: DEFAULT_LIMIT, videoId},{
+    const { data, isLoading}  = trpc.playlists.getManyForVideo.useInfiniteQuery({limit: DEFAULT_LIMIT, videoId},{
         getNextPageParam: (lastPage) => lastPage.nextCursor,
         enabled: !!videoId && open,
     })
@@ -30,6 +31,11 @@ export const PlaylistAddModel = ({
             onOpenChange={onOpenChange}
         >
           <div className="flex flex-col gap-2">
+            {isLoading && (
+                <div className="flex justify-center p-4">
+                    <Loader2Icon className="size-5 animate-spin text-muted-foreground" />
+                </div>
+            )}
             {JSON.stringify(data)}
           </div>
         </ResponsiveModal>
