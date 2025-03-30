5ca194806a129d135095c0d1987f70372231f10d
Joeic <cqy1508460399@gmail.com>
2025-03-30 15:12:21 -0600

    add image to playlists

--- src/modules/playlists/ui/sections/playlists-section.tsx
+++ src/modules/playlists/ui/sections/playlists-section.tsx
@@ -47,7 +47,7 @@ const PlaylistsSectionSuspense = () => {
 
     return(
         <div>
-            <div className="flex flex-col gap-4 gap-y-10 ">
+            <div className="gap-4 gap-y-10 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 [@media(min-width:1920px)]:grid-cols-6">
                 {playlists.pages.flatMap( (page) => page.items).map( (playlist) => (
                     <PlaylistGridCard data={playlist} key={playlist.id}/>
                 ))}
