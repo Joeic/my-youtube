5f75cb0a9e6a8401a7f6d7ff181374c83bebd700
Joeic <cqy1508460399@gmail.com>
2025-03-31 12:28:00 -0600

    add get and remove playlist to procedure

--- src/modules/playlists/server/procedures.ts
+++ src/modules/playlists/server/procedures.ts
@@ -9,6 +9,48 @@ import { useId } from "react";
 
 
 export const playlistsRouter = createTRPCRouter({
+    remove:protectedProcedure.input(
+        z.object({
+           id: z.string().uuid(),
+        }),
+    ).mutation(async ({ctx, input}) => {
+        const {id} = input;
+        const {id: userId} = ctx.user;
+
+        const [deletedPlaylist] = await db
+            .delete(playlists)
+            .where(and(
+                eq(playlists.id, id),
+                eq(playlists.userId, userId)
+            ))
+            .returning();
+
+        return deletedPlaylist;
+    }),
+
+    getOne:protectedProcedure.input(
+        z.object({
+           id: z.string().uuid(),
+        }),
+    ).query(async ({ctx, input}) => {
+        const {id} = input;
+        const {id: userId} = ctx.user;
+
+        const [existingPlaylist] = await db
+            .select()
+            .from(playlists)
+            .where(and(
+                eq(playlists.id, id),
+                eq(playlists.userId, userId)
+            ))
+        if(!existingPlaylist){
+            throw new TRPCError({code: "NOT_FOUND"});
+        }
+
+        return existingPlaylist;
+    })
+
+    ,
     getVideos:protectedProcedure.input(
         z.object({
             playlistId: z.string().uuid(),
