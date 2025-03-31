ca04aac3925a0eb4b237ced546c8c1b85d2971fd
Joeic <cqy1508460399@gmail.com>
2025-03-31 12:20:14 -0600

    add trash button

---
+++ src/modules/playlists/ui/sections/playlist-header-section.tsx
@@ -0,0 +1,42 @@
+import { Button } from "@/components/ui/button";
+import { Trash2Icon } from "lucide-react";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+
+interface PlaylistHeaderSectionProps {
+    playlistId: string;
+}
+
+export const PlaylistHeaderSection = ({playlistId}: PlaylistHeaderSectionProps) => {
+    return(
+        <Suspense fallback={<p>...Loading</p>} >
+            <ErrorBoundary fallback={<p>Error</p>} >
+                <PlaylistHeaderSectionSuspense playlistId={playlistId} />
+            </ErrorBoundary>
+        </Suspense>
+    )
+}
+
+const PlaylistHeaderSectionSuspense = ({playlistId}: PlaylistHeaderSectionProps) => {
+
+    return(
+        <div className=" flex justify-between items-center">
+            <div>
+            <h1 className=" text-2xl font-bold">
+                History
+            </h1>
+            <p className=" text-xs text-muted-foreground">
+                Videos from playlist
+            </p>
+           </div>
+           <Button
+                variant="outline"
+                size="icon"
+                className=" rounded-full"
+           >
+            <Trash2Icon />
+            
+           </Button>
+        </div>
+    )
+}
--- src/modules/playlists/ui/views/videos-view.tsx
+++ src/modules/playlists/ui/views/videos-view.tsx
@@ -1,4 +1,5 @@
 import { HistoryVideosSection } from "../sections/history-videos-section"
+import { PlaylistHeaderSection } from "../sections/playlist-header-section";
 
 interface VideosViewProps {
     playlistId: string;
@@ -7,14 +8,7 @@ interface VideosViewProps {
 export const VideosView = ({playlistId}: VideosViewProps) => {
     return (
         <div className="max-w-[2400px mx-auto mb-10 px-4 pt-2.5 flex flex-col gap-y-6">
-           <div>
-            <h1 className=" text-2xl font-bold">
-                Custom 
-            </h1>
-            <p className=" text-xs text-muted-foreground">
-                Custom playlist
-            </p>
-           </div>
+           <PlaylistHeaderSection playlistId={playlistId} />
            <HistoryVideosSection />
         </div>
     )
