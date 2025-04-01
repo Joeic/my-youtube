d5a127b39bd4dea4f583c773582c255edd43aa1a
Joeic <cqy1508460399@gmail.com>
2025-04-01 11:32:06 -0600

    add getOne in users router

---
+++ src/modules/users/server/procedures.ts
@@ -0,0 +1,43 @@
+import { db } from "@/db";
+import { subscriptions, users, videos } from "@/db/schema";
+import { baseProcedure, createTRPCRouter } from "@/trpc/init";
+import {z} from "zod";
+import {eq, and, or,lt,desc, getTableColumns, inArray, isNotNull} from "drizzle-orm";
+import { TRPCError } from "@trpc/server";
+
+
+export const usersRouter = createTRPCRouter({
+    getOne: baseProcedure
+    .input(z.object({id:z.string().uuid()}))
+    .query(async ({input,ctx}) => {
+        const {clerkUserId} = ctx;
+        let userId;
+        const [user] = await db 
+            .select()
+            .from(users)
+            .where(inArray(users.clerkId, clerkUserId ? [clerkUserId] : []))
+        if(user){
+            userId = user.id;
+        }
+        
+        const viewerSubscriptions = await db.$with("viewer_subscriptions").as(
+            db.select().from(subscriptions).where(inArray(subscriptions.viewerId, userId ? [userId] : []))
+        );
+        const [existingUser] = await db
+                                .with(viewerSubscriptions)
+                                .select({
+                                   ...getTableColumns(users),
+                                   viwerSubscribed: isNotNull(viewerSubscriptions.viewerId).mapWith(Boolean),
+                                   videoCount: db.$count(videos,eq(videos.userId, users.id)),
+                                   subscriberCount: db.$count(subscriptions,eq(subscriptions.creatorId, users.id)),
+                                })
+                                .from(users)
+                                .leftJoin(viewerSubscriptions, eq(viewerSubscriptions.creatorId, users.id))
+                                .where(eq(users.id, input.id))
+                                
+        if(!existingUser){
+            throw new TRPCError({code: "NOT_FOUND"});
+        }
+        return existingUser;
+    }),
+});
