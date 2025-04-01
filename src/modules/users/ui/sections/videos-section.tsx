31e29f586dba19f7353015ffff955426979449cf
Joeic <cqy1508460399@gmail.com>
2025-04-01 15:34:48 -0600

    delete unnecessary elements

--- src/modules/users/ui/sections/videos-section.tsx
+++ src/modules/users/ui/sections/videos-section.tsx
@@ -23,7 +23,8 @@ export const VideosSection = (props: VideosSectionProp) => {
 
 const HomeVideoSectionSkeleton =() => {
     return(
-        <div className="gap-4 gap-y-10 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 [@media(min-width:1920px)]:grid-cols-6">
+        <div className="gap-4 gap-y-10 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 
+            2xl:grid-cols-4">
             {Array.from({length: 18}).map( (_,index) => (
             <VideoGridCardSkeleton key={index} />
             ))}
@@ -40,7 +41,8 @@ const VideosSectionSuspense = ({userId}: VideosSectionProp) => {
 
     return(
         <div>
-            <div className="gap-4 gap-y-10 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 [@media(min-width:1920px)]:grid-cols-6">
+            <div className="gap-4 gap-y-10 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 
+            2xl:grid-cols-4">
                 {videos.pages.flatMap( (page) => page.items).map( (video) => (
                 <VideoGridCard key={video.id} data={video}  />
                 ))}
