4a9db16e2675360021d6d43e4cc352a73bd339c4
Joeic <cqy1508460399@gmail.com>
2025-04-01 11:34:04 -0600

    create user page

---
+++ src/app/(home)/users/[userId]/page.tsx
@@ -0,0 +1,26 @@
+import { DEFAULT_LIMIT } from "@/constans";
+import { VideoView } from "@/modules/videos/ui/views/video-view";
+import { trpc } from "@/trpc/server";
+
+export const dynamic = 'force-dynamic';
+interface PageProps{
+    params: Promise<{
+        videoId: string;
+    }>;
+}
+
+
+const Page = async ({ params }:PageProps) => {
+    const {videoId} = await params;
+
+    void trpc.videos.getOne.prefetch({id: videoId});
+    void trpc.comments.getMany.prefetchInfinite({videoId, limit: DEFAULT_LIMIT});
+    void trpc.suggestions.getMany.prefetchInfinite({videoId, limit: DEFAULT_LIMIT});
+    return (
+        <div>
+           UserPage
+        </div>
+    );
+};
+
+export default Page;
